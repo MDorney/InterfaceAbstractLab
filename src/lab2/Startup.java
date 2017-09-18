321f88988b26803d0891095cf60dbce64fe7ec80
Doern <Doern@DESKTOP-U7BLEN8>
2017-09-18 14:57:54 -0500

    Added comments to startup in accordance with requirements

--- src/lab2/Startup.java
+++ src/lab2/Startup.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This was a poor usage of inheritance in my opinion.  There is so much reuse of methods.
+ * Because I do not expect you to get a course number differently for a different type of course,
+ * I cannot see why you would possibly want to make these common methods abstract and require an override.
+ * For the second question, you cannot use Liskov Substitution if you intend to get prerequisites (while also using the enhanced for loop)
+ * Technically it would not throw an error if you ensured to check that each course is a member of a class that contains that method, but that is overkill.
+ * You could also add the methods to the interface and simply throw a new UnsupportedOperationException (In the child classes without prerequisites),
+ * although I do not know if that is best practice
  */
 package lab2;
 
